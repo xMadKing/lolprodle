188ebd2554b2668ad35abcbf8e2305c05999f6f2
Sulaxan <sulaxan33@yahoo.ca>
2023-06-26 17:26:28 -0400

    change dark theme, tab width

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+    content: ["./src/**/*.{html,js,svelte,ts}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+
+    daisyui: {
+        themes: true,
+        darkTheme: "dracula",
+        base: true,
+        styled: true,
+        utils: true,
+        rtl: false,
+        prefix: "",
+        logs: true,
+    },
 }
 
