972d66ce6e00515396c0a59a5682c5c6b21368a5
xmadking <ahmad.kh.se@outlook.com>
2023-06-27 06:57:17 +0200

    Add input box (still unfinished)

---
+++ frontend/src/lib/Inputdiv.svelte
@@ -0,0 +1,6 @@
+<div class="form-control w-full max-w-xs">
+  <label class="label">
+    <span class="label-text">Enter player name</span>
+  </label>
+  <input type="text" placeholder="Player name..." class="input input-bordered input-primary input-lg w-full max-w-xs" />
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
 	import Footer from '$lib/Footer.svelte';
 	import Navbar from '$lib/Navbar.svelte';
+	import Inputdiv from '$lib/Inputdiv.svelte';
 </script>
 
 <Navbar />
 
+<Inputdiv />
 <p>some content</p>
 
 <Footer />
