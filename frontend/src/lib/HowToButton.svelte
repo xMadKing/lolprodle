68e77aa87ccf5a74bf63001e5f908ba4af15d363
Sulaxan <sulaxan33@yahoo.ca>
2023-06-27 12:44:36 -0400

    change how to info styling and add additional info

--- frontend/src/lib/HowToButton.svelte
+++ frontend/src/lib/HowToButton.svelte
@@ -1,14 +1,34 @@
 <div class="dropdown dropdown-end">
     <label tabindex="0" class="btn btn-circle btn-ghost btn-xs text-info">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 stroke-current">
-            <path stroke-linecap="square" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="w-4 h-4 stroke-current"
+        >
+            <path
+                stroke-linecap="square"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
         </svg>
     </label>
-    <div tabindex="0" class="card compact dropdown-content z-[1] shadow bg-base-100 rounded-box w-64">
+    <div
+        tabindex="0"
+        class="card compact dropdown-content z-[1] shadow bg-base-200 rounded-box w-64"
+    >
         <div class="card-body">
-            <h2 class="card-title">How to play</h2> 
-            <p>Everyday a new pro player is selected for each region. You can select the region by clicking one of the logos above. 
-            Your task is to figure out the player name and enter it in the box besides. 
+            <h2 class="card-title">How to Play</h2>
+            <p>
+                Everyday a new pro player is selected for each region. You can select the region by
+                clicking one of the regions above. Your task is to figure out the player and enter
+                it in the box to the left.
+            </p>
+            <br />
+            <br />
+            <p class="text-warning">
+                Note that only <u>active</u> players and their current information are considered.
             </p>
         </div>
     </div>
