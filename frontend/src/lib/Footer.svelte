db1b42c6199d62dbf4a6f893a2b47ed878565e97
xmadking <ahmad.kh.se@outlook.com>
2023-06-27 21:46:34 +0200

    Fixed footer

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="footer footer-center p-4 bg-base-300 text-base-content fixed bottom-0">
+<footer class="footer footer-center p-4 bg-base-300 text-base-content">
 	<div>
 		<p>Copyright &copy; 2023 lolprodle.com</p>
 	</div>
