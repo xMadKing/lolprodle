6dfe7af9309ed1f346a53faffd604b2aa332788a
Sulaxan <sulaxan33@yahoo.ca>
2023-06-30 17:04:49 -0400

    remove manual redirects

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -7,25 +7,7 @@
     import Timer from "$lib/Timer.svelte";
     import Previous from "$lib/Previous.svelte";
     import GuessBox from "$lib/guess/GuessBox.svelte";
-    import { onMount } from "svelte";
-    import { REGION_DATA } from "$lib/types";
-    import { selectedRegion } from "$lib/stores";
     import background from "$lib/assets/background.webp";
-
-    // auto select region based on url
-    onMount(() => {
-        const urlComponents = window.location.href.split("#");
-        if (urlComponents.length < 2) {
-            return;
-        }
-
-        REGION_DATA.forEach((value, key) => {
-            if (value.slugs.indexOf(urlComponents[1]) !== -1) {
-                $selectedRegion = key;
-                return;
-            }
-        });
-    });
 </script>
 
 <div class="bg-no-repeat bg-cover" style="background-image: url({background});">
