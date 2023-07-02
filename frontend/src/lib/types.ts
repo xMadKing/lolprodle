2536e1768632f545c934890499a93d3e5be65bdb
Sulaxan <sulaxan33@yahoo.ca>
2023-07-01 22:00:52 -0400

    improve toast handler, move intervals into onMount in reset timer

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -44,11 +44,21 @@ export enum ToastStatus {
 export class Toast {
     readonly status: ToastStatus;
     readonly message: string;
-    readonly lifeEndTimeMillis: number; // unix time for when to hide
+    readonly durationMillis: number; // unix time for when to hide
 
-    constructor(status: ToastStatus, message: string, lifeEndTimeMillis: number) {
+    constructor(status: ToastStatus, message: string, durationMillis: number) {
         this.status = status;
         this.message = message;
-        this.lifeEndTimeMillis = lifeEndTimeMillis;
+        this.durationMillis = durationMillis;
+    }
+}
+
+export class Duration {
+    static secs(secs: number): number {
+        return secs * 1000;
+    }
+
+    static mins(mins: number): number {
+        return mins * this.secs(60);
     }
 }
