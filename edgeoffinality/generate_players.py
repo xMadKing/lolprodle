28c2506c3ab5f79fcf059eff7b13f7ecb6535baa
xmadking <ahmad.kh.se@outlook.com>
2023-07-02 23:08:03 +0200

    Subs no longer are included in the active players list

--- edgeoffinality/generate_players.py
+++ edgeoffinality/generate_players.py
@@ -2,6 +2,7 @@ from mwrogue.esports_client import EsportsClient
 from player import Player
 import get_teams as get_teams
 
+
 def generate_players(teams, region):
 
     site = EsportsClient("lol")
@@ -9,13 +10,19 @@ def generate_players(teams, region):
 
     for team in teams:
         results = site.cargo_client.query(
-            tables="Players",
-            fields="ID, Name, Team, Role, Country, FavChamps, IsSubstitute",
-            where='Team="{}"'.format(team) + " AND IsSubstitute = 0",
-            having="Role='Top' OR Role='Jungle' OR Role='Mid' OR Role='Bot' OR Role='Support'"
+            tables="TournamentPlayers=T, Players=P",
+            join_on="T.Team = P.Team",
+            fields="P.ID, P.Name, T.Team, P.Role, P.Country, P.FavChamps",
+            where='T.Player = P.ID AND P.Team = "{}" AND T.Team = "{}"'.format(
+                team, team),
+            having="P.Role = 'Mid' OR P.Role = 'Top' OR P.Role = 'Jungle' OR P.Role = 'Mid' OR P.Role = 'Bot' OR P.Role = 'Support'"
         )
         for i in results:
-            response.append(Player(i['ID'], i['Name'], i['Role'], i['Team'], i['Country'], str(i['FavChamps']).split(',')).toJSON())
+            if response.__contains__(Player(i['ID'], i['Name'], i['Role'], i['Team'], i['Country'], str(i['FavChamps']).split(',')).toJSON()):
+                pass
+            else:
+                response.append(Player(i['ID'], i['Name'], i['Role'], i['Team'], i['Country'], str(
+                    i['FavChamps']).split(',')).toJSON())
 
     with open('storage/{}_players.json'.format(region), 'w') as output:
         output.write('[')
@@ -24,10 +31,11 @@ def generate_players(teams, region):
             if i != len(response)-1:
                 output.write(',\n')
         output.write(']')
-    
+
     return "success"
 
+
 generate_players(get_teams.get_lec_teams(), 'lec')
-generate_players(get_teams.get_lcs_teams(), 'lcs')
 generate_players(get_teams.get_lck_teams(), 'lck')
+generate_players(get_teams.get_lcs_teams(), 'lcs')
 generate_players(get_teams.get_lpl_teams(), 'lpl')
--- edgeoffinality/player.py
+++ edgeoffinality/player.py
@@ -1,8 +1,9 @@
 import json
+import html
 
 class Player:
     def __init__(self, id, name, role, team, country, fav_champs):
-        self.name = name
+        self.name = html.unescape(html.unescape(name)) #???? idek why we need to unscape twice, but this fixs the issue of unscaped names
         self.role = role
         self.team = team
         self.id = id
