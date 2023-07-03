84d350719752bb266f8893594e8d68735b71563f
Sulaxan <sulaxan33@yahoo.ca>
2023-07-03 01:08:08 -0400

    improve card colour

--- frontend/src/lib/guess/GuessCategoryCard.svelte
+++ frontend/src/lib/guess/GuessCategoryCard.svelte
@@ -2,12 +2,16 @@
     import { GUESS_CATEGORY_DATA, type PlayerGuessCategory } from "$lib/types";
 
     export let guessCategory: PlayerGuessCategory;
+
+    $: categoryClasses = guessCategory.correct ? 
+        "bg-gradient-to-br from-green-500 to-green-700" :
+        "bg-gradient-to-br from-red-400 to-red-500";
 </script>
 
 <div
-    class="card shadow-xl bg-green-500 border-2 border-base-100 hover:lg:transform hover:lg:-translate-y-2"
-    class:bg-green-500={guessCategory.correct}
-    class:bg-red-400={!guessCategory.correct}
+    class="card shadow-xl bg-green-500 border-2 border-base-100 hover:lg:transform
+    hover:lg:-translate-y-2 {categoryClasses}"
+
 >
     <div class="w-36 card-body text-black break-words">
         <h2 class="font-bold uppercase">
